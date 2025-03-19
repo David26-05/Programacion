1410939f6036e6509ddbba5395f633404c30eb0c
WILLIANS <da.garciaa@uea.edu.ec>
2025-03-19 05:13:26 -0500

    Añadida función calcular_descuento y pruebas correspondientes

--- CalculoDescuentoPhyton.py
+++ CalculoDescuentoPhyton.py
@@ -4,9 +4,9 @@ def calcular_descuento(monto_total, porcentaje_descuento=10):
     return descuento, monto_final
 
 # Primera llamada con solo el monto total
-descuento1, monto_final1 = calcular_descuento(100)
+descuento1, monto_final1 = calcular_descuento(100,10)
 print(f"Descuento: {descuento1}, Monto final: {monto_final1}")
 
 # Segunda llamada con monto total y porcentaje de descuento
-descuento2, monto_final2 = calcular_descuento(200, 20)
+descuento2, monto_final2 = calcular_descuento(200,20)
 print(f"Descuento: {descuento2}, Monto final: {monto_final2}")
